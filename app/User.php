245cb5009ee32bc2fa7161f3d0e1f2f860d36f4e
mapkoc88 <mjc93@njit.edu>
2018-11-26 01:56:44 -0500

    Task - Questions Migrate - Model - Test

--- app/User.php
+++ app/User.php
@@ -40,4 +40,9 @@ class User extends Authenticatable
     {
         return $this->hasOne('App\Profile');
     }
+
+    public function questions()
+    {
+        return $this->hasMany('App\Question');
+    }
 }
